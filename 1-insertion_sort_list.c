c171901844d38a121f529deb849d4b1a9e9af4dc
Adriana Ariza <1296@holbertonschool.com>
2020-02-03 11:09:31 -0500

    [Fix] Betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,41 +1,19 @@
 #include "sort.h"
-
+/**
+ *insertion_sort_list - sort list
+ *@list: lsit to sort
+ *Return: void
+ */
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *aux, *_tmp, *_prev, *_next;
-/**	int n_aux;
 
 	aux = *list;
-
-	while (aux && aux->next)
+	while (aux->n < aux->next->n)
 	{
-		if (aux->n < aux->next->n)
-		{
-			aux = aux->next;
-		}
-		else
-		{
-			n_aux = aux->n;
-			*(int *)&aux->n = (int)aux->next->n;
-			*(int *)&aux->next->n = n_aux;
-			print_list(*list);
-			while(aux->next && aux->prev && aux->n < aux->prev->n)
-			{
-				aux = aux->prev;
-				n_aux = (int)aux->n;
-				*(int *)&aux->n = (int)aux->next->n;
-				*(int *)&aux->next->n = n_aux;
-				print_list(*list);
-			}
-		}
-	}
-
-*/
-	aux = *list;
-	while(aux->n < aux->next->n){
 		aux = aux->next;
-		if(!aux->next)
-			return ;
+		if (!aux->next)
+			return;
 	}
 	_tmp = aux->prev;
 	_prev = aux->next;
@@ -44,11 +22,12 @@ void insertion_sort_list(listint_t **list)
 	_prev->next = aux;
 	aux->next = _next;
 	aux->prev = _prev;
-	if(_tmp == NULL){
+	if (_tmp == NULL)
+	{
 		_next->prev = aux;
 		*list = _prev;
 	}
-	else if(_next == NULL)
+	else if (_next == NULL)
 		_tmp->next = _prev;
 	else
 	{

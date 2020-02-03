abbafa48d672584c01ff319cb115c336a4d82681
Adriana Ariza <1296@holbertonschool.com>
2020-02-03 10:53:27 -0500

    [Fix]: Betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,21 +1,33 @@
 #include "sort.h"
+/**
+ *unsorted - checks if array is unsorted
+ *@array: array
+ *@size: array size
+ *Return: 1-unsorted, 0-sorted
+ */
 int unsorted(int *array, size_t size)
 {
 	int i  = 0;
 
-	for (i = 0; i < (size-2); i++)
+	for (i = 0; i < (size - 2); i++)
 	{
 		if (array[i] > array[i + 1])
 			return (1);
 	}
 	return (0);
 }
+/**
+ *bubble_sort - sort array
+ *@array: array
+ *@size: array size
+ *Return: 1-unsorted, 0-sorted
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i = 0;
 	int aux = 0;
 
-	while(_unsorted(array, size))
+	while (_unsorted(array, size))
 	{
 		for (i = 0; i < (size - 1) && _unsorted(&array[i]); i++)
 		{
@@ -26,10 +38,8 @@ void bubble_sort(int *array, size_t size)
 				array[i + 1] = aux;
 				print_array(array, size);
 			}
-			else{
+			else
 				;
-
-			}
 		}
 	}
 }

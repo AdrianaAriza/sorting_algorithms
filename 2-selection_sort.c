3459741d84f3ca47ccafa9ff3e881aef96d4d002
Adriana Ariza <1296@holbertonschool.com>
2020-02-03 11:18:22 -0500

    [Fix] Betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,11 +1,16 @@
 #include "sort.h"
-
+/**
+ *selection_sort - sort array
+ *@array: array to sort
+ *@size: array size
+ *Return: void
+*/
 void selection_sort(int *array, size_t size)
 {
 	size_t i = 0, j = 0;
 	int aux = 0, pos;
 
-	for(i = 0; i < size - 3; i++)
+	for (i = 0; i < size - 3; i++)
 	{
 		aux = array[i];
 		for (j = i; j < size; j++)

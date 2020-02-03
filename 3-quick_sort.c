874d7cd6deea2ead463b0c546061c8fd176cea5e
Adriana Ariza <1296@holbertonschool.com>
2020-02-03 11:37:15 -0500

    [Fix] Betty3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,16 +1,27 @@
 #include "sort.h"
-
+/**
+ *unsorted - checks if is unsorted
+ *@array: array
+ *@size: size
+ *Return: void
+ */
 int unsorted(int *array, size_t size)
 {
 	size_t i  = 0;
 
-	for (i = 0; i < (size-2); i++)
+	for (i = 0; i < (size - 2); i++)
 	{
 		if (array[i] > array[i + 1])
 			return (1);
 	}
 	return (0);
 }
+/**
+ *quick_sort - checks if is unsorted
+ *@array: array
+ *@size: size
+ *Return: void
+ */
 void quick_sort(int *array, size_t size)
 {
 	int pivot_swap = 0;
@@ -20,7 +31,7 @@ void quick_sort(int *array, size_t size)
 	while (unsorted(array, size))
 	{
 		i = 0;
-		while(array[i] < *piv)
+		while (array[i] < *piv)
 			i++;
 		if (i < (size - 1) && (&array[i] != piv))
 		{
@@ -28,7 +39,7 @@ void quick_sort(int *array, size_t size)
 			j = i + 1;
 			while (array[j] && *piv <= array[j])
 				j++;
-			if(j >= size)
+			if (j >= size)
 			{
 				array[i] = *piv;
 				*piv = pivot_swap;
@@ -45,5 +56,4 @@ void quick_sort(int *array, size_t size)
 		else
 			piv--;
 	}
-	return;
 }
